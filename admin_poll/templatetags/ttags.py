49274abe78c95b7e9cdc5235ad0f0ad6e4189dee
diliplj <45902226+diliplj@users.noreply.github.com>
2022-01-21 16:24:53 +0530

    latest templates with profile
    latest templates with profile

--- admin_poll/templatetags/ttags.py
+++ admin_poll/templatetags/ttags.py
@@ -8,3 +8,7 @@ register = template.Library()
 @register.filter(name='addcss')
 def addcss(field, css ):
     return field.as_widget(attrs={"class":css,"placeholder":field.name})
+
+@register.filter(name='addcss_label')
+def addcss_label(field, css ):
+    return field.as_widget(attrs={"class":css})
